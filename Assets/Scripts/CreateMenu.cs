d9290039e7490046986034a2631fce1d2dde154d
Mateusz Put <mateusz.put@gmail.com>
2016-04-17 16:58:34 +0200

    menu - ready

--- Assets/Scripts/CreateMenu.cs
+++ Assets/Scripts/CreateMenu.cs
@@ -3,41 +3,42 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class CreateMenu : MonoBehaviour {
-	
 	public GUIStyle titleStyle = new GUIStyle();
 	public GUIStyle buttonStyle = new GUIStyle();
+	public GUIStyle exitButtonStyle = new GUIStyle();
 
 	void OnGUI () {
 		titleStyle.font = (Font)Resources.Load("MagicSchoolTwo");
-		//titleStyle.fontSize = 50;
-
-		buttonStyle.font = (Font)Resources.Load("MagicSchoolTwo");
-		//buttonStyle.fontSize = 40;
-
-		GUI.color = Color.red;
 		GUI.Label (new Rect (20, 20, 120, 50), "Ucieczka z zaczarowanego lasu", titleStyle);
 
+		buttonStyle.font = (Font)Resources.Load("MagicSchoolTwo");
+		exitButtonStyle.font = (Font)Resources.Load("MagicSchoolTwo");
+
 		GUI.color = Color.yellow;
+		Rect layerSelectPos = new Rect(30, 120, 120, 40);
+
+		Event curEvent = Event.current;
+		if (layerSelectPos.Contains(curEvent.mousePosition))
+			buttonStyle.normal.textColor = Color.white;
+		else
+			buttonStyle.normal.textColor = Color.red;
+
+
 		if (GUI.Button (new Rect (30, 120, 120, 40), "Play", buttonStyle)) {
-			Debug.Log("Start button clicked");
-			//Application.LoadLevel ("infterr");
 			SceneManager.LoadScene("infterr");
 		}
-			
 
-		if (GUI.Button (new Rect (30, 180, 120, 40), "Exit", buttonStyle)) {
-			Debug.Log("End button clicked");
+		GUI.color = Color.yellow;
+		layerSelectPos = new Rect(30, 180, 120, 40);
+		if (layerSelectPos.Contains(curEvent.mousePosition))
+			exitButtonStyle.normal.textColor = Color.white;
+		else
+			exitButtonStyle.normal.textColor = Color.red;
+		
+		if (GUI.Button (new Rect (30, 180, 120, 40), "Exit", exitButtonStyle)) {
+			Application.Quit ();
 		}
 
 	}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
