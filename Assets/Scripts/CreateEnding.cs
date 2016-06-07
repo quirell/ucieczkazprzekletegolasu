673146e73e1d02955b7628d8d12924b250cded30
Gabriela Konstanty <gaba.konstanty@gmail.com>
2016-06-07 11:51:49 +0200

    add ugly ending

---
+++ Assets/Scripts/CreateEnding.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CreateEnding : MonoBehaviour {
+	public GUIStyle titleStyle = new GUIStyle();
+	public GUIStyle menuStyle = new GUIStyle();
+	
+	void OnGUI () {
+		titleStyle.font = (Font)Resources.Load("MagicSchoolTwo");
+		GUI.Label (new Rect (20, 20, 120, 50),
+                "Gratulacje! Wygrałeś! Uciekłeś z przerażającego, przeklętego lasu!", 
+                titleStyle);
+
+		menuStyle.font = (Font)Resources.Load("MagicSchoolTwo");
+	
+		GUI.color = Color.yellow;
+		Rect layerSelectPos = new Rect(30, 120, 120, 40);
+
+		Event curEvent = Event.current;
+		if (layerSelectPos.Contains(curEvent.mousePosition))
+			menuStyle.normal.textColor = Color.white;
+		else
+			menuStyle.normal.textColor = Color.red;
+
+
+		if (GUI.Button (new Rect (30, 120, 120, 40), "Menu", menuStyle)) {
+			SceneManager.LoadScene("menu");
+		}
+
+		GUI.color = Color.yellow;
+		layerSelectPos = new Rect(30, 180, 120, 40);
+	
+	}
+
+}
---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider other) {
+        if (other.transform.root.gameObject.tag == "player") {
+            
+            SceneManager.LoadScene("ending");
+        }
+
+    }
+
+}
