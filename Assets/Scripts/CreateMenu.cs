964cdb1c4cee4f8132157c2b6fcb8a56c878ad9b
Gabriela Konstanty <gaba.konstanty@gmail.com>
2016-06-07 15:09:07 +0200

    edit menu a little

--- Assets/Scripts/CreateMenu.cs
+++ Assets/Scripts/CreateMenu.cs
@@ -8,34 +8,37 @@ public class CreateMenu : MonoBehaviour {
 	public GUIStyle exitButtonStyle = new GUIStyle();
 
 	void OnGUI () {
-		titleStyle.font = (Font)Resources.Load("MagicSchoolTwo");
-		GUI.Label (new Rect (20, 20, 120, 50), "Ucieczka z zaczarowanego lasu", titleStyle);
+        
+        GUI.Box (new Rect (0, 30, Screen.width, 70), " ");
+
+        titleStyle.font = (Font)Resources.Load("MagicSchoolTwo");
+        GUI.Box (new Rect (0, 40, Screen.width, 40), "Ucieczka z zaczarowanego lasu", titleStyle);
 
 		buttonStyle.font = (Font)Resources.Load("MagicSchoolTwo");
 		exitButtonStyle.font = (Font)Resources.Load("MagicSchoolTwo");
 
-		GUI.color = Color.yellow;
-		Rect layerSelectPos = new Rect(30, 120, 120, 40);
+        GUI.color = Color.yellow;
+        Rect layerSelectPos = new Rect(0, 140, Screen.width, 40);
 
 		Event curEvent = Event.current;
 		if (layerSelectPos.Contains(curEvent.mousePosition))
 			buttonStyle.normal.textColor = Color.white;
 		else
-			buttonStyle.normal.textColor = Color.red;
+            buttonStyle.normal.textColor = Color.red;
 
 
-		if (GUI.Button (new Rect (30, 120, 120, 40), "Play", buttonStyle)) {
+        if (GUI.Button (new Rect (0, 140, Screen.width, 40), "Play", buttonStyle)) {
 			SceneManager.LoadScene("infterr");
 		}
 
-		GUI.color = Color.yellow;
-		layerSelectPos = new Rect(30, 180, 120, 40);
+        GUI.color = Color.yellow;
+        layerSelectPos = new Rect(0, 200, Screen.width, 40);
 		if (layerSelectPos.Contains(curEvent.mousePosition))
 			exitButtonStyle.normal.textColor = Color.white;
 		else
-			exitButtonStyle.normal.textColor = Color.red;
+            exitButtonStyle.normal.textColor = Color.red;
 		
-		if (GUI.Button (new Rect (30, 180, 120, 40), "Exit", exitButtonStyle)) {
+        if (GUI.Button (new Rect (0, 200, Screen.width, 40), "Exit", exitButtonStyle)) {
 			Application.Quit ();
 		}
 
